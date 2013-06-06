540a7a0d7e3173abf7510c4525133a38c286fbba
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2013-06-06 14:46:52 -0500

    updates to libquat
    mostly that it actually works now... I think. someone test please.

--- quat.cpp
+++ quat.cpp
@@ -11,61 +11,84 @@ Quat::Quat()
 
 Quat::Quat(double angle, Vtx axis)
 {
-  w=cos(DEG2RAD(angle)/2);
+  Quat q;
+  q.w=cos(DEG2RAD(angle)/2);
   double s=sin(DEG2RAD(angle)/2);
-  Vtx v=axis.normalize()*s;
-  x=v.x;
-  y=v.y;
-  z=v.z;
+  Vtx n=axis.normalize()*s;
+  q.x=n.x;
+  q.y=n.y;
+  q.z=n.z;
+  //  q=q.normalize();
+  w=q.w;
+  x=q.x;
+  y=q.y;
+  z=q.z;
+}
+
+Quat::Quat(Vtx v)
+{
+  Vtx n=v.normalize();
+  w=0;
+  x=n.x;
+  y=n.y;
+  z=n.z;
 }
 
 Quat::Quat(double vw, double vx, double vy, double vz)
 {
-  w=vw;
-  x=vx;
-  y=vy;
-  z=vz;
+  Quat q(vw,Vtx(vx,vy,vz));
+  w=q.w;
+  x=q.x;
+  y=q.y;
+  z=q.z;
 }
 
 Quat::~Quat(){}
 
+
+
 double Quat::magnitude()
 {
-  return sqrt(w*w+x*x+y*y+z*z);
+  return sqrt(w*w + x*x + y*y + z*z);
 }
 
-void Quat::normalize()
+Quat Quat::normalize()
 {
   double m=magnitude();
-  w=w/m;
-  x=x/m;
-  y=y/m;
-  z=z/m;
+  Quat q;
+  q.w=w/m;
+  q.x=x/m;
+  q.y=y/m;
+  q.z=z/m;
+  return q;
 }
 
 Quat Quat::operator=(Vtx v)
 {
-  return Quat(0,v.x,v.y,v.z);
+  return Quat(v);
 }
 
-Vtx Quat::vtx()
-{
-  return Vtx(x,y,z);
-}
 
-Quat Quat::operator*(Quat q)
+Quat Quat::multiply(Quat q)
 {
   Quat r;
   r.w = w*q.w - x*q.x - y*q.y - z*q.z;
   r.x = w*q.x + x*q.w + y*q.z + z*q.y;
   r.y = w*q.y - x*q.z + y*q.w + z*q.x;
   r.z = w*q.z + x*q.y - y*q.x + z*q.w;
-  return r;
+  
+  //return r;
+  return r.normalize(); //should normalize?
+}
+
+Quat Quat::operator*(Quat q)
+{
+  return multiply(q);
 }
 
 Quat Quat::operator*(Vtx v)
 {
-  return Quat(0,x*v.x,y*v.y,z*v.z);
+  return multiply(Quat(v));
 }
 
 Quat Quat::operator*(double s)
@@ -75,11 +98,35 @@ Quat Quat::operator*(double s)
   r.x*=s;
   r.y*=s;
   r.z*=s;
-  return r; 
+  return r;
+  //  return multiply(Quat(s,0,0,0)); //more elegant / accurate?
 }
 
 Vtx Quat::rotate(Vtx v)
+{ //rotate a 3D vector by a quaternion
+  Quat q(w,-x,-y,-z); //q^(-1)
+  Quat r=this->multiply(Quat(v)); //qv
+  r=r.multiply(q); //(qv)q^(-1)
+  return Vtx(r.x,r.y,r.z); //is correct?
+}
+
+double Quat::angle() //in degrees
 {
-  Quat q(w,-x,-y,-z);
-  return (Quat(w,x,y,z)*v*q).vtx();
+  return Acos(w)*2.0;
+}
+
+Vtx Quat::axis()
+{
+  Vtx r;
+  double s=sqrt(1.0 - w*w);
+  if(s==0.0) { //eh?
+    r.x=x;
+    r.y=y;
+    r.z=z;
+    } else { 
+    r.x=x/s;
+    r.y=y/s;
+    r.z=z/s;
+  } 
+  return r.normalize();
 }
--- quat.h
+++ quat.h
@@ -1,7 +1,7 @@
 #ifndef QUAT_H
 #define QUAT_H 1
 
-#include "config.h"
+//#include "config.h"
 #include <vtx/vtx.h>
 #include <cmath>
 using namespace std;
@@ -10,13 +10,16 @@ class Quat {
  public:
   double w,x,y,z;
   Quat(double angle, Vtx axis); //axis/angle initialization
+  Quat(Vtx v); //quaternion representation of a vector
   Quat(double w, double x, double y, double z); //raw initialization (w,x,y,z)
   Quat(); //identity (1,0,0,0)
   ~Quat();
   double magnitude();
-  void normalize();
+  Quat normalize();
   Quat operator=(Vtx v);
-  Vtx vtx();
+  Vtx axis();
+  double angle();
+  Quat multiply(Quat q);
   Quat operator*(Quat q);
   Quat operator*(Vtx v);
   Quat operator*(double s);
